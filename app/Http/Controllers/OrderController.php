d215059852117d61ad5d3f5e12e98d1f0344e42d
Md. Azharul Islam <azi@fiftytwo.com>
2025-04-15 15:47:11 +0600

    storing data in cache

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request;
 use App\Models\Order;
+use Illuminate\Support\Facades\Cache;
 
 class OrderController extends Controller
 {
     public function index(){
-        $orders = Order::with('customer')->paginate(10);
+        //$orders = Order::with('customer')->paginate(5);
+        $page = Request::get('page', 1); // current page or default to 1
+        $cacheKey = "orders_page_{$page}";
+        $orders = Cache::remember($cacheKey, now()->addMinutes(5), function () {
+            return Order::with('customer')->paginate(5);
+        });
         return view('orders.index', compact('orders'));
     }
     public function show(Order $order){
